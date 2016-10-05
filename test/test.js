87687a30e7ac98207df55b1ba84ebc6a8aab341d
Giuseppe Futia <giuseppe.futia@gmail.com>
2016-10-05 18:39:50 +0200

    Improve tests with updated data

--- test/test.js
+++ test/test.js
@@ -1,40 +1,43 @@
 var fs = require('fs'),
     assert = require('assert'),
-    mongo = require('../mongo/mongo.js'),
+    mongo = require('../mongo/mongo.js');
     docs = [],
     collection = '',
-    fields = {}
+    fields = {},
     key = {};
 
 before(function () {
     fs.readFile('test/data/photo-test.json', function (err, data) {
         if (err) throw err;
-        docs.push(JSON.parse(data));
+        var items = JSON.parse(data)["items"];
+        for (var i in items) {
+            docs.push(items[i]);
+        }
     });
     collection = 'photo-test';
-    fields = { 'urlstore': 1, 'urlthumb':1 };
-    key = {'urlthumb': 'http://cdn1-odm.sviluppoaptservizi.com/odm/r,'};
+    fields = {'id': 1, 'autore': 1, 'linkorig':1 };
+    key = {'id': '1071'};
 });
 
 describe('Test Mongo', function () {
     it('should add new documents', function (done) {
         mongo.writeInDB(docs, collection, function (result) {
             assert.deepEqual(result['result']['ok'], 1);
-            assert.deepEqual(result['result']['n'], 1);
+            assert.deepEqual(result['result']['n'], 2);
             done();
         });
     });
     it('should list all documents in a collection with specific fields', function (done) {
         mongo.listFromDB(collection, fields, function (list) {
-            assert.deepEqual(list[0]['urlstore'], 'http://photo.aptservizi.com/store/');
-            assert.deepEqual(list[0]['urlthumb'], 'http://cdn1-odm.sviluppoaptservizi.com/odm/r,');
-            assert.deepEqual(list[0]['version'], undefined);
+            assert.deepEqual(list[0]['autore'], 'Pascual gargiulo');
+            assert.deepEqual(list[0]['linkorig'], 'https://commons.wikimedia.org/wiki/File:Piazza_del_Popolo_e_Residenza_Comunale_-_039014204-MIBAC_-.JPG');
+            assert.deepEqual(list[0]['soggetto'], undefined);
             done();
         });
     });
-    it('should return specific fields of defined document', function (done) {
+    it('should return specific fields of a defined document', function (done) {
         mongo.getDoc(collection, fields, key, function (item) {
-            assert.deepEqual(item['urlstore'], 'http://photo.aptservizi.com/store/');
+            assert.deepEqual(item['autore'], 'Federfabbri');
             done();
         });
     });
