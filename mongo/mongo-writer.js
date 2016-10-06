195311e2e2c52fb84cec450e4184fa73335d4f34
Giuseppe Futia <giuseppe.futia@gmail.com>
2016-10-06 11:38:12 +0200

    Write real data in mongo

---
+++ mongo/mongo-writer.js
@@ -0,0 +1,34 @@
+var  fs = require('fs');
+var mongo = require('../mongo/mongo.js');
+var resources = {};
+
+fs.readFile('mongo-writer-conf.json', function (err, data) {
+    if (err) throw err;
+    resources = JSON.parse(data);
+    prepareData();
+});
+
+var prepareData = function () {
+    for (var i in resources['resources']) {
+        writeCollectionInDB(resources['resources'][i]);
+    }
+}
+
+var writeCollectionInDB = function (collection) {
+    var path = collection['path'],
+        name = collection['name'],
+        iterator = collection['iterator'],
+        fields = collection['fields'],
+        enabled = collection['enabled'];
+
+    if (!enabled) return;
+
+    fs.readFile(path, function (err, data) {
+        if (err) throw err;
+        var docs = JSON.parse(data)[iterator];
+        mongo.writeInDB(docs, name, function (result) {
+            console.log(docs.length);
+            console.log(name + " written in DB");
+        });
+    });
+}
