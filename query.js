ceef760cc127d22f4631559e9d1ebdb6b4b00800
Simone Basso <bassosimone@gmail.com>
2016-10-08 18:24:31 +0200

    query.js: changes after code review
    To test that everything was fine I run the /api/geo/nearest
    query and make sure it returned something meaningful.

--- query.js
+++ query.js
@@ -1,63 +1,75 @@
 var https = require('https');
-var host = "4ac10947-aef3-43fa-ab73-6f212fc11b4b-bluemix.cloudant.com"; //N.B. non ci va http o https
+var me = '4ac10947-aef3-43fa-ab73-6f212fc11b4b-bluemix'; // Your account
+var host = me + ".cloudant.com"; // Note: no URL schema needed here
 var querystring = require('querystring');
 var Cloudant = require('cloudant');
-var me = '4ac10947-aef3-43fa-ab73-6f212fc11b4b-bluemix'; // Replace with your account
+
+// Note: apparently it would be fine for this to be undefined if you have
+// set up a database without any read restriction
 var password = process.env.cloudant_password;
+
+// TODO: perhaps always use this easier library to make requests
 var requestlib = require("request");
 
+// TODO: refactor this callback to take error as first argument
 exports.listDocsType = function (type, cb) {
-  Cloudant({account:me, password:password}, function(err, cloudant) {
+  Cloudant({account:me, password:password}, function (err, cloudant) {
     if (err) {
+      // XXX here it would be better to callback(err)
       return console.log('Failed to initialize Cloudant: ' + err.message);
     }
     var db = cloudant.db.use("scoprira");
-    db.find({selector:{"properties": {"type": type}}}, function(err, data) {
-      console.log("listDocsType" + data);
-      cb(data.docs)
+    db.find({selector:{"properties": {"type": type}}}, function (err, data) {
+      console.log("listDocsType:", data);
+      // TODO: do we need to check for `docs` being undefined here?
+      cb(data.docs);
     });
   });
-}
+};
 
 exports.getDoc = function (id, cb) {
+  // TODO: we can perhaps factor out the login as a common function
   Cloudant({account:me, password:password}, function(err, cloudant) {
     if (err) {
       return console.log('Failed to initialize Cloudant: ' + err.message);
     }
     var db = cloudant.db.use("scoprira");
     db.get(id, function(err, data) {
-      console.log("getDoc" + data);
-      cb(data)
+      console.log("getDoc", data);
+      cb(data);
     });
   });
-}
+};
 
+// XXX I believe the following function to depend on the database to
+// contain an index for geographical data. Use Cloudant's web interface
+// to create said index because doing it from node is not simple
 exports.getLocations = function (request, cb) {
-  Cloudant({account:me, password:password}, function(err, cloudant) {
+  Cloudant({account:me, password:password}, function (err, cloudant) {
     if (err) {
       return console.log('Failed to initialize Cloudant: ' + err.message);
     }
-    //var db = cloudant.db.use("scoprira");
-    //db.geo('places', 'GeoIndex', request, function(err, data) {
-    //https://4ac10947-aef3-43fa-ab73-6f212fc11b4b-bluemix.cloudant.com/scoprira/_design/places/_geo/GeoIndex?g=point(29+-95)&limit=20&nearest=true
+    // Note: code adapted from Cloudant's own API
+    // Note: we rolled out our own implementation because we failed to
+    // use Cloudant's own API to successfully make the request
     var url = "https://" + encodeURIComponent(host) + "/scoprira/_design/" +
                encodeURIComponent("places") + "/_geo/" +
                encodeURIComponent("GeoIndex") + "?" +
                querystring.stringify({
                  nearest: true,
                  limit: request.limit,
-                 g: "point("+request.lat+" "+request.lon+")"
+                 g: "point(" + request.lat + " " + request.lon + ")"
                });
-    console.log("url", url)
+    console.log("url", url);
     requestlib(url, function (error, response, body) {
       console.log("error ", error);
       //console.log("response ", response);
       console.log("body ", body);
       //console.log("geo points " + data);
-      cb(body); // XXX
+      cb(body);
     });
   });
-}
+};
 
 exports.launchQuery = function (request, response, query, acceptFormat) {
     var options = {
@@ -67,16 +79,14 @@ exports.launchQuery = function (request, response, query, acceptFormat) {
         method: "GET"
     };
 
-
-    var req = https.request(options, function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
+    var req = https.request(options, function (res) {
+        console.log('STATUS: ', res.statusCode);
+        console.log('HEADERS: ', res.headers);
         res.setEncoding('utf8');
         var result = "";
         res.on('data', function(chunk) {
             result += chunk;
         });
-
         res.on('end', function() {
             response.send(result);
         });
@@ -84,7 +94,7 @@ exports.launchQuery = function (request, response, query, acceptFormat) {
 
     req.on('error', function(e) {
         console.log('problem with request: ' + e.message);
+        // XXX perhaps here we should callback
     });
-
     req.end();
-}
+};
