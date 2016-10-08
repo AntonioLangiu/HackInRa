045ecbe329e102787b0fa629ab40a99ce1633683
Simone Basso <bassosimone@gmail.com>
2016-10-08 18:13:18 +0200

    server.js: lightweight code review

--- server.js
+++ server.js
@@ -1,50 +1,36 @@
 var express = require('express');
 var query = require('./query.js');
 var apicache = require('apicache').options({ debug: true }).middleware;
-var bodyParser = require('body-parser');
 var querystring = require('querystring');
 var port = process.env.VCAP_APP_PORT || 8080;
 var oneDay = 86400000;
 var app = express();
 
-app.use('/',express.static('./website/static', { maxAge: oneDay }));
-//app.use(bodyParser.json());
+app.use('/', express.static('./website/static', { maxAge: oneDay }));
 
-/* PAGES */
-app.get('/about', function (request, response) {
-    response.sendFile(__dirname + '/about.html');
-});
-
-
-/* QUERY */
 app.get('/api/all/:type', apicache('1 day'), function (request, response) {
-   query.listDocsType(request.params.type, function (data){
+   query.listDocsType(request.params.type, function (data) {
        response.writeHead(200, {"Content-Type": "application/json"});
        response.end(JSON.stringify(data));
    });
 });
 
-app.get('/api/doc/:id',apicache('1 day'), function (request, response) {
-   query.getDoc(request.params.id, function (data){
+app.get('/api/doc/:id', apicache('1 day'), function (request, response) {
+   query.getDoc(request.params.id, function (data) {
        response.writeHead(200, {"Content-Type": "application/json"});
        response.end(JSON.stringify(data));
    });
 });
 
 app.get('/api/geo/nearest', apicache('1 day'), function (request, response) {
-   console.log(request.query);
-   query.getLocations(request.query, function (data){
+   query.getLocations(request.query, function (data) {
        response.writeHead(200, {"Content-Type": "application/json"});
-       response.end(JSON.stringify(JSON.parse(data)["rows"])); // XXX
+       // XXX The following would throw exception if `data` is invalid
+       response.end(JSON.stringify(JSON.parse(data)["rows"]));
    });
 });
 
-
-app.use(function(req, res) {
-    res.status(404).sendFile(__dirname + '/404.html');
-});
-
-var server = app.listen(port, function() {
+var server = app.listen(port, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Server listening at http://%s:%s', host, port);
